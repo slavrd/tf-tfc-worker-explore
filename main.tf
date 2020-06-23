8860cc5fe89d0b370b01d7b1f8fe09ebf7cb1ce7
Slav Donchev <slav.donchev@gmail.com>
2020-06-23 09:54:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ resource "null_resource" "files" {
   provisioner "local-exec" {
     command = "find | sed 's|[^/]*/|- |g'"
   }
+  triggeres = {
+    run_every_time = uuid()
+  }
 }
 
 resource "null_resource" "env" {
   provisioner "local-exec" {
     command = "env"
+  }
+  triggeres = {
+    run_every_time = uuid()
   }
 }
 
@@ -14,11 +20,17 @@ resource "null_resource" "cat-backend-override" {
   provisioner "local-exec" {
     command = "cat zzz_backend_override.tf.json"
   }
+  triggeres = {
+    run_every_time = uuid()
+  }
 }
 
 resource "null_resource" "mem-check" {
   provisioner "local-exec" {
     command = "cat /proc/meminfo | grep 'MemTotal'"
+  }
+  triggeres = {
+    run_every_time = uuid()
   }
 }
 
