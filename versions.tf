516c70de3aa67304f632a2ce0868ed5727119f3b
Slav Donchev <slav.donchev@gmail.com>
2021-12-16 10:36:01 +0200

    fix formatting

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-     null = {
-        source = "registry.terraform.io/hashicorp/null"
-	version = "~> 3.0"
-     }
+    null = {
+      source  = "registry.terraform.io/hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
